3d1a1d431ead68f129626160efc27bc2cf9f7ba1
Marco Zorzi <marcozo91@gmail.com>
2016-04-16 20:42:28 +0200

    changed file name and included pwm driver library

---
+++ auto_trax_io/src/auto_trax_io_node.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv){
+    //Init ros and create node handle
+    ros::init(argc, argv, "auto_trax_io_node");
+    ros::NodeHandle nh;
+
+    std::cout << "Hello world!" << std::endl;
+
+    ros::Rate r(50); // 50 hz
+    while (nh.ok())
+    {
+        ros::spinOnce();
+        r.sleep();
+    }
+
+return 0;
+}
