6df2571dca076ec5b357fd4b81c49e4b987be411
Pavel <pvechersky@gmail.com>
2016-04-24 10:46:24 +0200

    Finishing segmentation (test mode for now)

--- auto_trax_sensors/src/line_tracking.cpp
+++ auto_trax_sensors/src/line_tracking.cpp
@@ -53,7 +53,7 @@ void LineTracking::ImageCallback(const sensor_msgs::ImageConstPtr &image_msg) {
   std::vector<cv::Vec4i> lines;
   cv::HoughLinesP(cv_ptr->image, lines, 1, CV_PI/180, 50, 50, 10);
 
-  cv::Vec4i closest_left_line;
+  /*cv::Vec4i closest_left_line;
   double max_negative_slope = -10.0;
 
   for (size_t i = 0; i < lines.size(); i++) {
@@ -91,12 +91,12 @@ void LineTracking::ImageCallback(const sensor_msgs::ImageConstPtr &image_msg) {
 
   // Publish the occupancy grid
   occ_grid->header.frame_id = "map";
-  occ_grid_pub_.publish(*occ_grid);
+  occ_grid_pub_.publish(*occ_grid);*/
 }
 
 void LineTracking::Test() {
   cv::Mat test_image;
-  test_image = cv::imread("/home/pavel/bike-path.jpg", CV_LOAD_IMAGE_COLOR);
+  test_image = cv::imread("/home/pavel/tape-track.jpg", CV_LOAD_IMAGE_COLOR);
 
   if (!test_image.data ) {
     std::cout <<  "Could not open or find the image" << std::endl ;
@@ -107,45 +107,86 @@ void LineTracking::Test() {
   cv::Rect area_to_crop(0, test_image.rows - horizon_pixels_, test_image.cols, horizon_pixels_);
   cv::Mat cropped = test_image(area_to_crop);
 
+  // Detect all the pixels within the desired RGB interval
   cv::Mat dest;
   cv::inRange(cropped, lower_bound_, upper_bound_, dest);
 
-  cv::Mat hough(dest.rows, dest.cols, CV_8UC3, cv::Scalar(0,0,0));
+  // Canny edge detector
+  cv::Mat canny;
+  cv::Canny(dest, canny, 80, 250);
+
+  cv::Mat hough(canny.rows, canny.cols, CV_8UC3, cv::Scalar(255,255,255));
   std::vector<cv::Vec4i> lines;
-  cv::HoughLinesP(dest, lines, 1, CV_PI/180, 50, 50, 10);
+  cv::HoughLinesP(canny, lines, 1, CV_PI/180, 50, 50, 10);
 
   cv::Vec4i closest_left_line;
+  cv::Vec4i closest_right_line;
   double max_negative_slope = -10.0;
+  double min_positive_slope = 10.0;
+  int left_wall_position = 0;
+  int right_wall_position = hough.cols;
+
+  cv::Mat hough_lines(canny.rows, canny.cols, CV_8UC3, cv::Scalar(0,0,0));
 
   for (size_t i = 0; i < lines.size(); i++) {
     cv::Vec4i l = lines[i];
     double slope = (double)(l[1] - l[3]) / (double)(l[0] - l[2]);
-    if (slope > max_negative_slope && slope < 0.0) {
+
+    //if (slope > max_negative_slope && slope < 0.0 && l[0] > left_wall_position) {
+    if (slope < 0.0 && (l[0] + l[2]) > (closest_left_line[0] + closest_left_line[2])) {
+      left_wall_position = l[0];
       max_negative_slope = slope;
       closest_left_line = l;
     }
+
+    //if (slope < min_positive_slope && slope > 0.0 && l[0] < right_wall_position) {
+    if (slope > 0.0 && l[0] < right_wall_position) {
+      right_wall_position = l[0];
+      min_positive_slope = slope;
+      closest_right_line = l;
+    }
+
+    cv::line(hough_lines, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
   }
 
-  if (cv::sum(closest_left_line).val > 0) {
+  //
+  // TODO: check if the lines were found
+  //
+  const cv::Scalar black(0, 0, 0);
+  int npt[] = { 4 };
+  int size_c = 1;
+
+    // Process left track
     int x_up = (0 - closest_left_line[1] + max_negative_slope * closest_left_line[0]) / max_negative_slope;
-    int x_down = (dest.rows - closest_left_line[1] + max_negative_slope * closest_left_line[0]) / max_negative_slope;
+    int x_down = (hough.rows - closest_left_line[1] + max_negative_slope * closest_left_line[0]) / max_negative_slope;
 
-    cv::Point occupied_points[1][4];
-    occupied_points[0][0] = cv::Point(0, 0);
-    occupied_points[0][1] = cv::Point(0, hough.rows);
-    occupied_points[0][2] = cv::Point(x_down, hough.rows);
-    occupied_points[0][3] = cv::Point(x_up, 0);
+    cv::Point left_wall[1][4];
+    left_wall[0][0] = cv::Point(0, 0);
+    left_wall[0][1] = cv::Point(0, hough.rows);
+    left_wall[0][2] = cv::Point(x_down, hough.rows);
+    left_wall[0][3] = cv::Point(x_up, 0);
 
-    const cv::Point* ppt[1] = {occupied_points[0]};
-    const cv::Scalar white(255, 255, 255);
-    int npt[] = { 4 };
-    int size_c = 1;
-    cv::fillPoly(hough, ppt, npt, size_c, white);
-  }
+    const cv::Point* left[1] = {left_wall[0]};
+    cv::fillPoly(hough, left, npt, size_c, black);
 
-  cv::imwrite("/home/pavel/cropped-path.jpg", cropped);
-  cv::imwrite("/home/pavel/dest-path.jpg", dest);
-  cv::imwrite("/home/pavel/hough-path.jpg", hough);
+    // Process right track
+    x_up = (0 - closest_right_line[1] + min_positive_slope * closest_right_line[0]) / min_positive_slope;
+    x_down = (hough.rows - closest_right_line[1] + min_positive_slope * closest_right_line[0]) / min_positive_slope;
+
+    cv::Point right_wall[1][4];
+    right_wall[0][0] = cv::Point(x_up, 0);
+    right_wall[0][1] = cv::Point(x_down, hough.rows);
+    right_wall[0][2] = cv::Point(hough.cols, hough.rows);
+    right_wall[0][3] = cv::Point(hough.cols, 0);
+
+    const cv::Point* right[1] = {right_wall[0]};
+    cv::fillPoly(hough, right, npt, size_c, black);
+
+  //cv::imwrite("/home/pavel/cropped-path-turned-a-lot.jpg", cropped);
+  //cv::imwrite("/home/pavel/thresholded-path-turned-a-lot.jpg", dest);
+  cv::imwrite("/home/pavel/segmented-path.jpg", hough);
+  //cv::imwrite("/home/pavel/canny-path-turned-a-lot.jpg", canny);
+  //cv::imwrite("/home/pavel/lines-path-turned-a-lot.jpg", hough_lines);
 
   cv::Mat im_gray;
   cv::cvtColor(hough, im_gray, CV_RGB2GRAY);
@@ -167,7 +208,7 @@ int main(int argc, char **argv) {
   //
   // Testing
   //
-  //line_tracking.Test();
+  line_tracking.Test();
 
   ros::spin();
 
