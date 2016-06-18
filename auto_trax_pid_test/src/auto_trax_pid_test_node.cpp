cbf02ddbd6e428680677283b8b48a35236721478
Pavel <pvechersky@gmail.com>
2016-06-18 18:57:26 +0200

    Adding ability to merge two laser scans into one

--- auto_trax_pid_test/src/auto_trax_pid_test_node.cpp
+++ auto_trax_pid_test/src/auto_trax_pid_test_node.cpp
@@ -7,12 +7,19 @@
 
 void InitializeParameters(const ros::NodeHandle& nh, ParameterBag* parameter) {
   // Retrieve all parameters or set to default
-  nh.param("subscribed_rostopic_img", parameter->subscribed_rostopic_img, kDefaultImageSubTopic);
-  nh.param("queue_size_subscriber_img", parameter->queue_size_subscriber_img, kDefaultImageSubQueueSize);
   nh.param("subscribed_rostopic_scan", parameter->subscribed_rostopic_scan, kDefaultScanSubTopic);
   nh.param("queue_subscribed_rostopic_scan", parameter->queue_size_subscriber_scan, kDefaultScanSubQueueSize);
   nh.param("pub_rostopic_dist", parameter->pub_rostopic_dist, kDefaultDistPubTopic);
   nh.param("queue_size_pub_dist", parameter->queue_size_pub_dist, kDefaultDistPubQueueSize);
+  nh.param("pub_rostopic_merged_scan", parameter->pub_rostopic_merged_scan, kDefaultMergedScanPubTopic);
+  nh.param("queue_size_pub_merged_scan", parameter->queue_size_pub_merged_scan, kDefaultMergedScanPubQueueSize);
+  nh.param("merged_scan_angle_increment", parameter->angle_increment, kDefaultAngleIncrement);
+  nh.param("frame_id_left", parameter->frame_id_left, kDefaultFrameIdLeft);
+  nh.param("frame_id_right", parameter->frame_id_right, kDefaultFrameIdRight);
+  nh.param("left_camera_offset", parameter->left_camera_offset, kDefaultLeftCameraOffset);
+  nh.param("left_camera_orientation", parameter->left_camera_orientation, kDefaultLeftCameraOrientation);
+  nh.param("right_camera_offset", parameter->right_camera_offset, kDefaultRightCameraOffset);
+  nh.param("right_camera_orientation", parameter->right_camera_orientation, kDefaultRightCameraOrientation);
 }
 
 int main (int argc, char** argv)
