d32b69614c210a09632024fcbd53ed907cc04e28
fulkast <fulkast@gmail.com>
2016-07-09 15:56:57 +0200

    added drive to centroid

--- auto_trax_path_planning/src/emergency_stop_node.cpp
+++ auto_trax_path_planning/src/emergency_stop_node.cpp
@@ -3,11 +3,9 @@
 int main (int argc, char** argv)
 {
     // Initialize ROS
-    ros::init (argc, argv, "auto_trax_path_planning");
+    ros::init (argc, argv, "auto_trax_emergency_stop");
     ros::NodeHandle nh;
 
-    // Construct class detection_processor with ros::NodeHandle and parameter structure
-//    auto_trax::ControllerProcessor processor(nh, parameter);
 
     // Spin
     ros::spin();
