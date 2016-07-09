abd13253de2a823c7530ed6349ff69d4345a96e2
Pavel <pvechersky@gmail.com>
2016-07-09 10:57:43 +0200

    Moving some functionality to the laser scan processor library

--- auto_trax_sensors/src/library/laser_scan_processor.cpp
+++ auto_trax_sensors/src/library/laser_scan_processor.cpp
@@ -6,11 +6,46 @@
 
 namespace auto_trax {
 
-    LaserScanProcessor::LaserScanProcessor(){}
+LaserScanProcessor::LaserScanProcessor(const sensor_msgs::LaserScanConstPtr &laser_scan) {
+  angle_min_ = laser_scan->angle_min;
+  angle_max_ = laser_scan->angle_max;
+  angle_increment_ = laser_scan->angle_increment;
 
-    float LaserScanProcessor::GetClosestRange(const std::vector<float> &ranges)
-    {
-        return *(std::min(ranges.begin(), ranges.end()));
+  range_min_ = laser_scan->range_min;
+  range_max_ = laser_scan->range_max;
+
+  ranges_ = laser_scan->ranges;
+}
+
+LaserScanProcessor::~LaserScanProcessor() {
+}
+
+float LaserScanProcessor::GetClosestRange(const std::vector<float> &ranges) {
+  return *(std::min(ranges.begin(), ranges.end()));
+}
+
+void LaserScanProcessor::GetMaxValidAngle(float& angle, int& ind) {
+  for (int i = ranges_.size() - 1; i >= 0; i--) {
+    float range = ranges_.at(i);
+
+    if (IsRangeValid(range)) {
+      angle = angle_min_ + i * angle_increment_;
+      ind = i;
+      break;
     }
+  }
+}
+
+void LaserScanProcessor::GetMinValidAngle(float& angle, int& ind) {
+  for (int i = 0; i < ranges_.size(); i++) {
+    float range = ranges_.at(i);
+
+    if (IsRangeValid(range)) {
+      angle = angle_min_ + i * angle_increment_;
+      ind = i;
+      break;
+    }
+  }
+}
 
 }
