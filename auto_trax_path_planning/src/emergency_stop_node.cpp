b5731d90856c27d34310ba73b915db5c3813be06
Marius <marius.grimm93@web.de>
2016-07-09 12:56:44 +0200

    changed to catkin_simple and package format 2.0

--- auto_trax_path_planning/src/emergency_stop_node.cpp
+++ auto_trax_path_planning/src/emergency_stop_node.cpp
@@ -1,16 +1,13 @@
-//
-
-//
-
+#include <ros/ros.h>
 
 int main (int argc, char** argv)
 {
     // Initialize ROS
-    ros::init (argc, argv, "auto_trax_emergency_stop_node");
+    ros::init (argc, argv, "auto_trax_path_planning");
     ros::NodeHandle nh;
 
     // Construct class detection_processor with ros::NodeHandle and parameter structure
-    auto_trax::ControllerProcessor processor(nh, parameter);
+//    auto_trax::ControllerProcessor processor(nh, parameter);
 
     // Spin
     ros::spin();
